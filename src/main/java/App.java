8cbc9f6a501054da162ff9f2aa0bf13a28682a5a
MrAlexSmith <mr.alexander.smith@hotmail.com>
2023-06-23 14:04:08 +0300

    Задача решена, тест пройден.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,11 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Сравнение бинов HelloWorld: " + (bean == bean));
+        System.out.println("Сравнение бинов Cat: " + (cat1 == cat2));
     }
 }
